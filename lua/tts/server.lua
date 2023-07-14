cdcb16a4f856e8042554fbe0b10057fc3f096ea1
KeepDrive <ipad2kirillo@gmail.com>
2023-07-14 19:58:07 +0300

    Server system rewrite

--- lua/tts/server.lua
+++ lua/tts/server.lua
@@ -1,49 +1,61 @@
 local uv = vim.uv
-local server
-local client
 
 local localhost = "127.0.0.1"
-local port = 39998
-
+local listener_port = 39998
+local sender_port = 39999
 local backlog = 5
 
-local function start_client()
-	client = uv.new_tcp()
+local public = {}
+
+local function start_client(server)
+	assert(server, "No server provided for client")
+	local client = uv.new_tcp()
 	server:accept(client)
-	client:read_start(function(err, data)
-		assert(not err, err)
-	end)
-	print("TTS server started")
+	return client
 end
 
-local function stop_client()
-	if client == nil then
+local function stop_client(client)
+	if not client then
 		return
 	end
 	client:shutdown()
 	client:close()
-	client = nil
 end
 
-local function start_server()
-	server = uv.new_tcp()
-	server:bind(localhost, port)
+local function get_server_client(server)
+	return getmetatable(server).client
+end
+
+function public.start_server(ip, port)
+	local server = uv.new_tcp()
+	server:bind(ip, port)
 	server:listen(backlog, function(err)
 		assert(not err, err)
-		start_client()
+		local client = start_client(server)
+		local server_metatable = { client = client }
+		setmetatable(server, server_metatable)
+	end)
+	return server
+end
+
+function public.stop_server(server)
+	if server == nil then
+		return
+	end
+	stop_client(get_server_client(server))
+	server:shutdown()
+	server:close()
+end
+
+function public.server_listen(server, reader)
+	get_server_client(server):read_start(function(err, data)
+		assert(not err, err)
+		reader(data)
 	end)
 end
 
-local function stop_server()
-	if server == nil then
-		print("No active TTS server")
-		return
-	end
-	stop_client()
-	server:shutdown()
-	server:close()
-	server = nil
-	print("TTS server stopped")
+function public.server_send(server, data)
+	get_server_client(server):write(data)
 end
 
-return { start_server = start_server, stop_server = stop_server }
+return public
