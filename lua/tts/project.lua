4fcaaca0e2be78878e0c87d7124a7a353e1b10d0
KeepDrive <ipad2kirillo@gmail.com>
2023-08-04 16:39:03 +0300

    Project script state getter/setter rewrite

--- lua/tts/project.lua
+++ lua/tts/project.lua
@@ -23,6 +23,10 @@ local function locate_project()
 	return locations and locations[1]
 end
 
+local function isDictEmpty(dict)
+	return not next(dict)
+end
+
 local function file_exists(path)
 	local file, err = io.open(path)
 	if err then
@@ -45,6 +49,30 @@ local function write_file(path, data)
 	assert(file, err)
 	assert(file:write(data))
 	file:close()
+end
+
+local FIND_SCRIPT = 1
+local FIND_UI = 2
+local FIND_SCRIPT_UI = 3
+local function find_and_process_moved_files(guid_type_table, process_match)
+	vim.fs.find(function(name, path)
+		local _, _, guid, type = script.process_file(path, false)
+		local object_status = guid_type_table[guid]
+		if
+			not object_status
+			or (type == "script" and object_status % 2 ~= 1)
+			or (type == "ui" and object_status < 2)
+		then
+			return false
+		end
+		object_status = object_status - (type == "script" and FIND_SCRIPT or FIND_UI)
+		guid_type_table[guid] = object_status
+		process_match(guid, type, path)
+		if object_status == 0 then
+			table.remove(guid_type_table, guid)
+		end
+		return isDictEmpty(guid_type_table)
+	end, { path = project_path })
 end
 
 local function set_project_path(path)
@@ -85,12 +113,8 @@ function public.load_project()
 	public.read_config()
 end
 
-local OBJECT_WRITE_SUCCESS = 0
-local OBJECT_WRITE_SCRIPT_FAILED = 1
-local OBJECT_WRITE_UI_FAILED = 2
-local OBJECT_WRITE_SCRIPT_UI_FAILED = 3
 function public.write_object(object)
-	local write_status = OBJECT_WRITE_SUCCESS
+	local write_status = 0
 	local object_config = project_config[object.guid]
 	if not object_config then
 		object_config = {}
@@ -99,7 +123,7 @@ function public.write_object(object)
 	object_config.name = object.name
 	if object.script then
 		if object_config.script and not file_exists(object_config.script) then
-			write_status = write_status + OBJECT_WRITE_SCRIPT_FAILED
+			write_status = write_status + FIND_SCRIPT
 		else
 			object_config.script = object_config.script or project_path .. "/" .. object.guid .. ".lua"
 			script.write_file(object_config.script, object.script, object.name, object.guid, "script")
@@ -107,7 +131,7 @@ function public.write_object(object)
 	end
 	if object.ui then
 		if object_config.ui and not file_exists(object_config.ui) then
-			write_status = write_status + OBJECT_WRITE_UI_FAILED
+			write_status = write_status + FIND_UI
 		else
 			object_config.ui = object_config.ui or project_path .. "/" .. object.guid .. ".xml"
 			script.write_file(object_config.ui, object.ui, object.name, object.guid, "ui")
@@ -115,6 +139,30 @@ function public.write_object(object)
 	end
 	object_config.updated = write_status ~= 0
 	return write_status
+end
+
+function public.get_object(guid)
+	local object = project_config[guid]
+	local state = { guid = guid, name = object.name }
+	local file_status = 0
+	local script_path = object.script
+	local ui_path = object.ui
+	if script_path and not file_exists(script_path) then
+		file_status = file_status + FIND_SCRIPT
+	end
+	if ui_path and not file_exists(ui_path) then
+		file_status = file_status + FIND_UI
+	end
+	if file_status ~= 0 then
+		return nil, file_status
+	end
+	if script_path then
+		state.script = script.process_file(script_path)
+	end
+	if ui_path then
+		state.ui = script.process_file(ui_path)
+	end
+	return state
 end
 
 function public.add_file_to_push(path)
@@ -142,73 +190,46 @@ end
 
 function public.get_script_states(get_all)
 	local script_states = {}
+	local files_to_find = {}
 	for guid, object in pairs(project_config) do
 		if get_all or object.updated then
-			local scriptCode
-			if object.script then
-				local script_file, err = io.open(object.script)
-				assert(script_file, err)
-				scriptCode = script_file:read("*a")
-				script_file:close()
-				assert(scriptCode, "Script read error")
+			local object_state, status = public.get_object(guid)
+			if status then
+				files_to_find[guid] = status
+			else
+				script_states[#script_states + 1] = object_state
 			end
-			local ui
-			if object.ui then
-				local ui_file, err = io.open(object.ui)
-				assert(ui_file, err)
-				ui = ui_file:read("*a")
-				assert(ui, "Script read error")
-			end
-			local state = { guid = guid, name = object.name }
-			if scriptCode then
-				state.script = scriptCode
-			end
-			if ui then
-				state.ui = ui
-			end
-			script_states[#script_states + 1] = state
 		end
 	end
+	find_and_process_moved_files(files_to_find, function(guid, type, path)
+		project_config[guid][type] = path
+		if files_to_find[guid] == 0 then
+			script_states[#script_states + 1] = public.get_object(guid)
+		end
+	end)
 	return script_states
 end
 
-local function isDictEmpty(dict)
-	return not next(dict)
-end
-
 function public.set_script_states(script_states)
-	local searchFileQueue = {}
+	local file_status_queue = {}
+	local guid_to_object = {}
 	for i = 1, #script_states do
 		local object = script_states[i]
 		local write_status = public.write_object(object)
 		if write_status ~= 0 then
-			searchFileQueue[object.guid] = { write_status, object }
+			file_status_queue[object.guid] = write_status
+			guid_to_object[object.guid] = object
 		end
 	end
-	if isDictEmpty(searchFileQueue) then
+	if isDictEmpty(file_status_queue) then
 		return
 	end
-	vim.fs.find(function(name, path)
-		local _, _, guid, type = script.process_file(path, false)
-		local object = searchFileQueue[guid]
-		if object then
-			if object[1] == OBJECT_WRITE_SCRIPT_UI_FAILED then
-				project_config[guid][type] = path
-				object[1] = object[1] - (type == "script" and OBJECT_WRITE_SCRIPT_FAILED or OBJECT_WRITE_UI_FAILED)
-			elseif type == "script" and object[1] == OBJECT_WRITE_SCRIPT_FAILED then
-				project_config[guid][type] = path
-				object[1] = object[1] - OBJECT_WRITE_SCRIPT_FAILED
-			elseif type == "ui" and object[1] == OBJECT_WRITE_UI_FAILED then
-				project_config[guid][type] = path
-				object[1] = object[1] - OBJECT_WRITE_UI_FAILED
-			end
-			if object[1] == OBJECT_WRITE_SUCCESS then
-				public.write_object(object[2])
-				table.remove(searchFileQueue, guid)
-			end
+	find_and_process_moved_files(file_status_queue, function(guid, type, path)
+		project_config[guid][type] = path
+		if file_status_queue[guid] == 0 then
+			public.write_object(guid_to_object[guid])
 		end
-		return isDictEmpty(searchFileQueue)
-	end, { path = project_path })
+	end)
 end
 
 return public
